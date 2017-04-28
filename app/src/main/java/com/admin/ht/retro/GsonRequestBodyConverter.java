8d10bcef254841c468246125a26e4caf18ec07f2
Solstice-Lin <649599725@qq.com>
2017-04-28 11:31:22 +0800

    Update GsonRequestBodyConverter.java

--- app/src/main/java/com/admin/ht/retro/GsonRequestBodyConverter.java
+++ app/src/main/java/com/admin/ht/retro/GsonRequestBodyConverter.java
@@ -2,14 +2,11 @@ package com.admin.ht.retro;
 
 import com.admin.ht.utils.LogUtils;
 import com.google.gson.Gson;
-
-
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.lang.reflect.Type;
 import java.nio.charset.Charset;
-
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
 import okio.Buffer;
@@ -17,12 +14,13 @@ import retrofit2.Converter;
 
 
 /**
+ * 自定义Requeset Gson解析器
+ *
  * Created by Spec_Inc on 4/16/2017.
  */
 public class GsonRequestBodyConverter<T> implements Converter<T, RequestBody> {
     private static final MediaType MEDIA_TYPE = MediaType.parse("application/json; charset=UTF-8");
     private static final Charset UTF_8 = Charset.forName("UTF-8");
-
     private final Gson gson;
     private final Type type;
 
@@ -36,14 +34,11 @@ public class GsonRequestBodyConverter<T> implements Converter<T, RequestBody> {
         Buffer buffer = new Buffer();
         Writer writer = new OutputStreamWriter(buffer.outputStream(), UTF_8);
         try {
-            LogUtils.e("Convert", "-------------------");
-            LogUtils.e("Convert", gson.toJson(String.class));
             gson.toJson(value, type, writer);
             writer.flush();
         } catch (IOException e) {
-            throw new AssertionError(e); // Writing to Buffer does no I/O.
+            throw new AssertionError(e); 
         }
         return RequestBody.create(MEDIA_TYPE, buffer.readByteString());
     }
-
 }
