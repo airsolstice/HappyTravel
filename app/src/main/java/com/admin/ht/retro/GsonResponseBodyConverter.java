91ddc6e62c05870b1d76998d16c241f69bccbfaa
Solstice-Lin <649599725@qq.com>
2017-04-28 11:34:13 +0800

    Update GsonResponseBodyConverter.java

--- app/src/main/java/com/admin/ht/retro/GsonResponseBodyConverter.java
+++ app/src/main/java/com/admin/ht/retro/GsonResponseBodyConverter.java
@@ -4,19 +4,18 @@ package com.admin.ht.retro;
 import com.admin.ht.utils.LogUtils;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
-
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.Type;
-
 import okhttp3.ResponseBody;
 import retrofit2.Converter;
 
 /**
+ * 自定义Response Gson解析器
+ *
  * Created by Spec_Inc on 4/16/2017.
  */
-
 public class GsonResponseBodyConverter<T> implements Converter<ResponseBody, T> {
     private final Gson gson;
     private final Type type;
@@ -28,23 +27,26 @@ public class GsonResponseBodyConverter<T> implements Converter<ResponseBody, T> 
 
     @Override
     public T convert(ResponseBody value) throws IOException {
-
         Reader reader = value.charStream();
         try {
             JsonReader jsonReader = new JsonReader(reader);
+            //设置gson宽松解析
             jsonReader.setLenient(true);
             return gson.fromJson(jsonReader, type);
-            // return gson.fromJson(reader, type);
+            //默认解析方式
+            //return gson.fromJson(reader, type);
         } finally {
             closeQuietly(reader);
         }
     }
 
     static void closeQuietly(Closeable closeable) {
-        if (closeable == null) return;
+        if (closeable == null) 
+            return;
         try {
             closeable.close();
         } catch (IOException ignored) {
+            
         }
     }
 }
